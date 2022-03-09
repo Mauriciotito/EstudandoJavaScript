e27425efa8cc5b700e80cdb67e16610c2290a83b
Maurício Santos <edutran.ms.98@gmail.com>
2022-03-09 18:15:49 -0300

    Exercicio 01 - site para hora

---
+++ aula12/exercicio01.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 9
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        //BOM DIA!
+        img.src = 'manhã.jpg'
+        document.body.style.background = '#EAEAAE'
+    }
+    else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#CD7F32'
+    }
+    else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#236B8E'
+    }
+}
