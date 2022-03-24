ddec9aaeee62e7ee5e840e35a362f6d030f5a9a4
gamertap <gamertap.games@gmail.com>
2022-03-23 21:10:57 -0300

    adicionado uma contagem decrescente

--- aula13ex/exercicio.js
+++ aula13ex/exercicio.js
@@ -5,13 +5,18 @@ function Contar() {
     var res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = '[ERROR] Número inicial não pode ser 0.'
+        res.innerHTML = '[ERROR] Algum campo inválido.'
     }
     else {
         res.innerHTML = 'Contando: '
         var ini = Number(inicio.value)
         var end = Number(fim.value)
         var pas = Number(passo.value)
+
+        if(pas <= 0){
+            alert('Passo inválido! Considerando passo 1...')
+            pas = 1
+        }
 
         if (ini < end) {
             for (var c = ini; c <= end; c += pas) {
